a95146cff5dd194eb5cca453a513a60fa7dea785
PotatoOff <christianabissi50@gmail.com>
2025-03-13 15:22:23 -0400

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Exllamav2</span>,
+  project: {
+    link: 'https://github.com/turboderp-org/exllamav2',
+  },
+  chat: {
+    link: 'https://discord.com/dsCjen9AUV',
+  },
+  docsRepositoryBase: 'https://github.com/turboderp-org/exllamav2',
+  footer: {
+    text: 'Exllamav2 Documentation',
+  },
+}
+
+export default config
